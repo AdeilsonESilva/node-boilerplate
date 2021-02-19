29143ba86528080ac0e47fc0e6174dcd537a5c95
Adeilson Silva <adeilson@gmail.com>
2021-02-19 09:11:56 -0300

    movido feature para v1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HeroesModule } from './features/heroes/heroes-module';
+import { HeroesModule } from './features/heroes/v1/heroes-module';
 
 @Module({
   imports: [HeroesModule],
