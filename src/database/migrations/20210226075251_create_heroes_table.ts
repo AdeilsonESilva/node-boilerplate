b2fb7b782e1b0626752ba1347211f200c53b599c
Adeilson Silva <adeilson@gmail.com>
2021-03-27 18:34:43 -0300

    Revert "configurado knex para usar o schema do servico e nao mais o public para gerenciar as migrations"
    This reverts commit 2c84dc1fd2e091b406ff4a8f4b9b9502f6c93a3e.

--- src/database/migrations/20210226075251_create_heroes_table.ts
+++ src/database/migrations/20210226075251_create_heroes_table.ts
@@ -1,16 +1,15 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema
-    .withSchema('heroservice')
-    .createTable('heroes', (table: Knex.TableBuilder) => {
-      table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-      table.string('name').notNullable();
-      table.boolean('enabled').notNullable();
-      table.string('race').notNullable();
-      table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
-      table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
-    });
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+  return knex.schema.createTable('heroes', (table: Knex.TableBuilder) => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('name').notNullable();
+    table.boolean('enabled').notNullable();
+    table.string('race').notNullable();
+    table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
