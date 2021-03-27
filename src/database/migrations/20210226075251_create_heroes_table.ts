f055e34baa4b2b4678ed50694655226852edc1b3
Adeilson Silva <adeilson@gmail.com>
2021-03-27 18:34:56 -0300

    Revert "adicionado function para uso do uuid nas migrations do knex"
    This reverts commit bfda4d9a81f9493bdca2c140bece3bd6be380a83.

--- src/database/migrations/20210226075251_create_heroes_table.ts
+++ src/database/migrations/20210226075251_create_heroes_table.ts
@@ -1,7 +1,6 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
   return knex.schema.createTable('heroes', (table: Knex.TableBuilder) => {
     table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
     table.string('name').notNullable();
